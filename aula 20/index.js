d9bd269841cad70be227a3821e0443ab8f27b461
eusejo <clancysla@gmail.com>
2025-10-09 10:22:23 -0300

    Reforço sobre tipos

---
+++ aula 20/index.js
@@ -0,0 +1,9 @@
+/*
+Primitivos (imutáveis) - string, number, boolean, null, undefined
+(bigint, symbol) - Valores copiados
+
+Referência (mutável) - array, object, function - Valores passados por referência
+*/
+
+
+
