7b015c090ee0a421e89144df90392d881d81e0ba
eusejo <clancysla@gmail.com>
2025-10-09 13:50:03 -0300

    operadores de comparação

---
+++ aula 22/index.js
@@ -0,0 +1,11 @@
+/*
+Operadores de comparação
+==	Igual (com coerção de tipo)	'1' == 1	true
+===	Estritamente igual (sem coerção de tipo)	'1' === 1	false
+!=	Diferente (com coerção de tipo)	'1' != 1	false
+!==	Estritamente diferente (sem coerção de tipo)	'1' !== 1	true
+>	Maior que	10 > 5	true
+<	Menor que	10 < 5	false
+>=	Maior ou igual a	10 >= 10	true
+<=	Menor ou igual a	10 <= 5	false
+*/
