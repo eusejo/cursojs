5380c57d9dacaace8229e0efcb71d6a9b1c8f34c
eusejo <clancysla@gmail.com>
2025-10-10 13:36:33 -0300

    switch case

---
+++ aula 29/index.js
@@ -0,0 +1,27 @@
+const data = new Date('1987-04-21 00:00:00')
+const diaSemana = data.getDay()
+let diaSemanaTexto;
+
+switch (diaSemana){
+    case 0:
+        diaSemanaTexto = 'Domingo'
+        break
+    case 0:
+        diaSemanaTexto = 'Segunda'
+        break
+    case 0:
+        diaSemanaTexto = 'Terça'
+        break
+    case 0:
+        diaSemanaTexto = 'Quarta'
+        break
+    case 0:
+        diaSemanaTexto = 'Quinta'
+        break
+    case 0:
+        diaSemanaTexto = 'Sexta'
+        break
+    case 0:
+        diaSemanaTexto = 'Sabado'
+        break
+}
